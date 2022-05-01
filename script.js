1984a786e03f86c2e37691feb60f054f59e86ed3
ErezAvni9 <NCC17019@protonmail.com>
2022-05-01 03:04:14 +0300

    adding starting project

---
+++ script.js
@@ -0,0 +1,44 @@
+let css = document.querySelector("h3")
+let color1 = document.querySelector(".color1")
+let color2 = document.querySelector(".color2")
+let body = document.getElementById("gradient")
+let randomButton = document.getElementById("random")
+
+function setGradient() {
+    body.style.background = 
+    "linear-gradient(to right, " 
+    + color1.value 
+    + ", " 
+    + color2.value 
+    + ")"
+
+    css.textContent = body.style.background + ";"
+}
+
+function randomColor() {
+  
+}
+function getRandomColor() {
+	let letters = '0123456789ABCDEF';
+	let color = '#';
+	for (let i = 0; i < 6; i++) {
+	  color += letters[Math.floor(Math.random() * 16)];
+	}
+	return color;
+  }
+
+
+  function randomColors(){
+	color1.value = getRandomColor();
+	color2.value = getRandomColor();
+
+	setGradient();
+}
+
+setGradient();
+
+color1.addEventListener("input", setGradient) 
+
+color2.addEventListener("input", setGradient)
+
+randomButton.addEventListener("click", randomColors);
